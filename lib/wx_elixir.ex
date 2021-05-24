144527bce270eabe0fcb4612b486bbfdd0825438
Grant Horner <h.grant.horner@gmail.com>
2021-05-23 20:10:29 -0400

    copy pasted code from example https://gist.github.com/rlipscombe/5f400451706efde62acbbd80700a6b7c

--- lib/wx_elixir.ex
+++ lib/wx_elixir.ex
@@ -1,18 +1,46 @@
 defmodule WxElixir do
-  @moduledoc """
-  Documentation for `WxElixir`.
-  """
+  # Copy pasted from https://gist.github.com/rlipscombe/5f400451706efde62acbbd80700a6b7c
+  @behaviour :wx_object
 
-  @doc """
-  Hello world.
+  @title "Canvas Example"
+  @size {600, 600}
 
-  ## Examples
+  def start_link(_opts) do
+    :wx_object.start_link(__MODULE__, [], [])
+  end
 
-      iex> WxElixir.hello()
-      :world
+  def init(args \\ []) do
+    wx = :wx.new
+    frame = :wxFrame.new(wx, -1, @title, size: @size)
+    :wxFrame.connect(frame, :size)
+    :wxFrame.connect(frame, :close_window)
 
-  """
-  def hello do
-    :world
+    panel = :wxPanel.new(frame, [])
+    :wxPanel.connect(panel, :paint, [:callback])
+
+    :wxFrame.show(frame)
+
+    state = %{panel: panel}
+    {frame, state}
+  end
+
+  def handle_event({:wx, _, _, _, {:wxSize, :size, size, _}}, state = %{panel: panel}) do
+    :wxPanel.setSize(panel, size)
+    {:noreply, state}
+  end
+
+  def handle_event({:wx, _, _, _, {:wxClose, :close_window}}, state) do
+    {:stop, :normal, state}
+  end
+
+  def handle_sync_event({:wx, _, _, _, {:wxPaint, :paint}}, _, state = %{panel: panel}) do
+    brush = :wxBrush.new
+    :wxBrush.setColour(brush, {255, 255, 255, 255})
+
+    dc = :wxPaintDC.new(panel)
+    :wxDC.setBackground(dc, brush)
+    :wxDC.clear(dc)
+    :wxPaintDC.destroy(dc)
+    :ok
   end
 end
